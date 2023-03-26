a0b38cfae993e6ba30b9f65f6104c446ceb00389
Bohdan Matsko <bm01271970@gmail.com>
2023-03-26 13:26:25 +0200

    Update task-02.js

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+liElements.forEach(element => {
+
+  const liElement = document.createElement('li');
+  liElement.textContent = element;
+  liElement.classList.add('item');
+});
+
+const allLiElements = document.querySelector('.ingredients');
+allLiElements.appendChild(liElements);
