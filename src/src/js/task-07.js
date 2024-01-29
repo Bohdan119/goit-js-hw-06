ba88e4b419b6f4e38023e0fa52189ad59f035a2e
Bohdan Matsko <bm01271970@gmail.com>
2024-01-29 21:23:36 +0100

    HW_JS_06__task_08

--- src/src/js/task-07.js
+++ src/src/js/task-07.js
@@ -1,1 +1,7 @@
-document.querySelector("#font-size-control");
+
+const range = document.querySelector("#font-size-control");
+const span = document.querySelector("#text");
+
+range.addEventListener("input", function(event) {
+    span.style.fontSize = this.value + "px";
+});
--- src/src/js/task-08.js
+++ src/src/js/task-08.js
@@ -1,1 +1,30 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", handlerForm)
+
+function handlerForm(evt) {
+    evt.preventDefault();
+    const formData = new FormData(evt.currentTarget);
+    // console.log(formData);
+    formData.forEach((value, name) => {
+        console.log('handlerForm -> name', name);
+        console.log('handlerForm -> value', value);
+        // console.log(formData);
+    });
+    const formElements = evt.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+    if
+        (email == ''){
+        alert('PLEASE WRITE EMAIL');
+    }
+    if
+        (password == ''){
+        alert('PLEASE WRITE PASSWORD');
+    }
+    evt.target.reset();
+}
+
+
+
 
